dfa18a8035ac062181a7ddd3599b43d5177e363b
lesnyrumcajs <lesny.rumcajs@gmail.com>
2018-10-23 20:54:43 +0200

    added documentation

--- src/math/gray_code.h
+++ src/math/gray_code.h
@@ -3,9 +3,21 @@
 #include <string>
 
 namespace cppchallenge::math {
+    /**
+     * @param number
+     * @return binary representation of a decimal
+     */
     std::string dec2binary(uint8_t number);
 
+    /**
+     * @param number
+     * @return gray representation of a decimal
+     */
     std::string dec2gray(uint8_t number);
 
+    /**
+     * @param gray
+     * @return decimal value of a grey representation
+     */
     uint8_t gray2dec(std::string gray);
 }
